b70e760a627bbd628dc74da9246f3ea4f93fd35a
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-14 14:40:17 +0530

    get data to employee overview

---
+++ PieShopHRM.App/Pages/EmployeeOverview.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using PieShopHRM.App.Models;
+
+namespace PieShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; } = default;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+
+
+        }
+
+    }
+}
