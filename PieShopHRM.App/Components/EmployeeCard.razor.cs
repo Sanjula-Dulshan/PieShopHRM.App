360cc3f356c7348c490510c1f393eaa4bf1fa18a
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-15 12:02:00 +0530

    create employee component with employee parameter

---
+++ PieShopHRM.App/Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace PieShopHRM.App.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+    }
+}
