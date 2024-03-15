2d1fb63e431119ec5642ba145c365181e932873e
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-15 13:47:58 +0530

    QuickViewPopup

---
+++ PieShopHRM.App/Components/QuickViewPopup.razor.cs
@@ -0,0 +1,10 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace PieShopHRM.App.Components
+{
+    public partial class QuickViewPopup
+    {
+        public Employee? Employee { get; set; }
+        private Employee? _employee;
+    }
+}
